1298a50feb5f8151910664e01c29427067b75a57
hoggenw <253192463@qq.com>
2018-02-18 21:49:39 +0800

    no message

---
+++ hello.lua
@@ -0,0 +1,20 @@
+print("hello".."world");
+-- a = 1;
+-- b = a * 2;
+-- print(b);
+
+function  fact( n )
+	-- body
+	if n == 0 then
+		return 1
+	else
+		return n * fact(n - 1)
+	end
+
+end
+
+print("enter a number")
+print(xx);
+
+a = io.read("number")
+print(fact(a))
